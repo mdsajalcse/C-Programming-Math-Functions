e226dcc1f5d545c50313842b822d6cbb71633c15
ifthekar <sajalcse2018@gmail.com>
2023-05-06 16:53:59 +0600

    floor function add

---
+++ floor.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    double f=-10.55;
+    int final;
+    final=floor(f);
+    printf("Floor Value is::%.2f=%d",f,final);
+
+    return 0;
+}
---
+++ floor_with_scanf_fun.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+       double userInputValue;
+       int result;
+       printf("Please input which value You Want to Convert Floor ::");
+       scanf("%lf",&userInputValue);
+       result=floor(userInputValue);
+       printf("Floor Value is::%lf=%d",userInputValue,result);
+
+
+return 0;
+}
