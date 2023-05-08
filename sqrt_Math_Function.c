cec0a708e1dc647e762c6295b6077b194e51bbed
ifthekar <sajalcse2018@gmail.com>
2023-05-08 15:09:13 +0600

    square root function program run successfully

---
+++ sqrt_Math_Function.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+       double value,value1,value2;
+       value=sqrt(25);
+       value1=sqrt(49);
+       value2=sqrt(8);
+       printf("25 square root is::%.2lf\n",value);
+       printf("49 square root is::%.2lf\n",value1);
+       printf("8 square root is::%.2lf\n",value2);
+
+
+
+return 0;
+}
