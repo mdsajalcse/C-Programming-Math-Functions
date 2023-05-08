b90e25b197728379e27e93b523fbd3edb768ed2d
ifthekar <sajalcse2018@gmail.com>
2023-05-08 16:23:47 +0600

    round program run successfully

---
+++ round.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+       double x, result;
+       printf("Enter value of X= ");
+       scanf("%lf",&x);
+       result=round(x);
+       printf("Round Function Value is::%.2lf\n",result);
+
+return 0;
+}
