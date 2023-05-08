08235b23dad051e439f3f8c1328d8ed8438e0f5e
ifthekar <sajalcse2018@gmail.com>
2023-05-08 15:19:17 +0600

    sqrt program with scanf

---
+++ sqrtWithScanf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+       int value;
+       double result;
+       printf("Enter a value which you want to result= ");
+       scanf("%d",&value);
+       result=sqrt(value);
+       printf("square root is::%.2lf\n",result);
+
+return 0;
+}
