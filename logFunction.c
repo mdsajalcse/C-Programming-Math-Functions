37096b28359958576178c37f75d3f30b8df21688
ifthekar <sajalcse2018@gmail.com>
2023-05-08 15:46:12 +0600

    log function

---
+++ logFunction.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+
+
+       double x, result;
+       printf("Enter value of X= ");
+       scanf("%lf",&x);
+       result=log(x);
+       printf("Log Function Value is::%.2lf\n",result);
+
+return 0;
+}
