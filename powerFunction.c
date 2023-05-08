bec155bf18cfbdeadd330f3bf3bc3abd50724466
ifthekar <sajalcse2018@gmail.com>
2023-05-08 15:28:09 +0600

    power function program run successfully

---
+++ powerFunction.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+       int x,y;
+       double result;
+       printf("Enter value of X= ");
+       scanf("%d",&x);
+       printf("Enter value of Y= ");
+       scanf("%d",&y);
+       result=pow(x,y);
+       printf("square root is::%.2lf\n",result);
+
+return 0;
+}
+
